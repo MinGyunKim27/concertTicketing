ad4e3b02d2931212695ac55e829a4378593c0896
김민균 <kmg02845@gmail.com>
2025-07-07 14:04:14 +0900

    Initial commit

---
+++ src/main/java/org/example/shopping_mall_overview/ShoppingMallOverviewApplication.java
@@ -0,0 +1,13 @@
+package org.example.shopping_mall_overview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingMallOverviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingMallOverviewApplication.class, args);
+    }
+
+}
